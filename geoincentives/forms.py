0f0f1fec999a8475ddb068717036b2e29063e42d
jweigel <jweigel@ebay.com>
2013-10-11 21:30:01 -0700

    Changed to django form.

--- geoincentives/forms.py
+++ geoincentives/forms.py
@@ -14,26 +14,76 @@ class UserLoginForm(ModelForm):
             'password': forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control', })
         }
 
-class SignupForm(ModelForm):
-    class Meta:
-        model = User
-        fields = (
-            'username', 'password', 'email', 'first_name', 'last_name',
-            'address', 'city', 'state', 'zipcode', 'school', 'birthdate'
-        )
-        widgets = {
-            'username': forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'password': forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'email': forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'first_name': forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'address': forms.TextInput(attrs={'placeholder': 'Address', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'city': forms.TextInput(attrs={'placeholder': 'City', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'state': forms.TextInput(attrs={'placeholder': 'State', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'zipcode': forms.TextInput(attrs={'placeholder': 'Zip Code', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'school': forms.TextInput(attrs={'placeholder': 'School', 'class': 'form-control GINGER_SOFATWARE_control', }),
-            'birthdate': forms.DateInput(format="%m/%d/%y", attrs={'placeholder': 'Birth Date mm/dd/yyyy', 'class': 'form-control GINGER_SOFATWARE_control', }),
-        }
+
+class StudentLoginForm(forms.Form):
+    username = forms.CharField(
+        max_length=96,
+        label='Username',
+        widget=forms.TextInput(attrs={'autocomplete': 'off', 'class': 'span5', 'required': ''}),
+    )
+    password = forms.CharField(
+        max_length=255,
+        label='Password',
+        widget=forms.PasswordInput(attrs={'autocomplete': 'off', 'class': 'span5', }),
+    )
+
+class SignupForm(forms.Form):
+    username = forms.CharField(
+        max_length=100,
+        label='Username',
+        widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    password = forms.CharField(
+        max_length=100,
+        label='Password',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    email = forms.CharField(
+        max_length=255,
+        label='Email',
+        widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    first_name = forms.CharField(
+        max_length=128,
+        label='First Name',
+        widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    last_name = forms.CharField(
+        max_length=128,
+        label='Last Name',
+        widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    address = forms.CharField(
+        max_length=255,
+        label='Address',
+        widget=forms.TextInput(attrs={'placeholder': 'Address', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    city = forms.CharField(
+        max_length=255,
+        label='City',
+        widget=forms.TextInput(attrs={'placeholder': 'City', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    state = forms.CharField(
+        max_length=30,
+        label='State',
+        widget=forms.TextInput(attrs={'placeholder': 'State', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    zipcode = forms.CharField(
+        max_length=5,
+        label='Zip Code',
+        widget=forms.TextInput(attrs={'placeholder': 'Zip Code', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    school = forms.CharField(
+        max_length=255,
+        label='School',
+        widget=forms.TextInput(attrs={'placeholder': 'School', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+    birthdate = forms.CharField(
+        max_length=100,
+        label='Birth Date',
+        widget=forms.DateInput(format="%m/%d/%y", attrs={'placeholder': 'Birth Date mm/dd/yyyy', 'class': 'form-control GINGER_SOFATWARE_control', }),
+    )
+
 
 
 
